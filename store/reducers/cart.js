93a0e8eee681bbb55e3980fe0383c90f65415279
ccrisolo <ccrisolo3@gmail.com>
2020-06-16 17:19:12 -0700

    167 started add item to cart function

---
+++ store/reducers/cart.js
@@ -0,0 +1,27 @@
+import { ADD_TO_CART } from '../actions/cart';
+import CartItem from '../../models/cart-item';
+
+const initialState = {
+    items: {},
+    totalAmount: 0,
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            const addedProduct = action.product;
+            const prodPrice = addedProduct.price;
+            const prodTitle = addedProduct.title;
+
+            if (items[addedProduct.id]) {
+                //already know the item is in cart
+            } else {
+                const newCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+                return {
+                    ...state,
+                    items: { ...state.items, [addedProduct.id]: newCartItem }
+                }
+            }
+    }
+    return state;
+};
