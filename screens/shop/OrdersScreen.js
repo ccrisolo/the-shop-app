9ab41a9b29a7e5053cd54466eb33cc2f792c26cb
ccrisolo <ccrisolo3@gmail.com>
2020-06-23 11:48:41 -0700

    173 finish half of adding side drawer and order screen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import { Text, View, StyleSheet, FlatList, Platform } from "react-native";
 import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../../component/UI/HeaderButton';
 
 const OrdersScreen = (props) => {
   //both orders in useSelector refer to the one in App.js and order.js reducer repectively
@@ -15,8 +17,22 @@ const OrdersScreen = (props) => {
   );
 };
 
-OrdersScreen.navigationOptions = {
-    headerTitle: 'Your Orders'
+OrdersScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Your Orders",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconSize={23}
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
