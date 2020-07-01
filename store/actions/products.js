5ce633d187e85435cdc6b523998de58134c5aaac
ccrisolo <ccrisolo3@gmail.com>
2020-07-01 14:09:56 -0700

    204 add update and delete

--- store/actions/products.js
+++ store/actions/products.js
@@ -18,7 +18,7 @@ export const fetchProducts = () => {
       );
 
       if (!response.ok) {
-        throw new Error('Something went wrong!')
+        throw new Error("Something went wrong!");
       }
 
       const resData = await response.json();
@@ -46,7 +46,15 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async (dispatch) => {
+    await fetch(
+      `https://the-shop-app-64312.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE",
+      }
+    );
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -91,13 +99,34 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    const response = await fetch(
+      `https://the-shop-app-64312.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+        }),
+      }
+    );
+
+    if(!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
