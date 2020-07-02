6da6eece3a23571b565f5e048432065bfa063551
ccrisolo <ccrisolo3@gmail.com>
2020-07-01 19:36:55 -0700

    206 storing orders on the server

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,37 @@
-export const ADD_ORDER = 'ADD_ORDER';
+export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  const date = new Date();
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://the-shop-app-64312.firebaseio.com/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+    
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        date: date,
+        items: cartItems,
+        amount: totalAmount,
+      },
+    });
   };
 };
